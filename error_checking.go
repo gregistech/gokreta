ef8c6d2f10001d4a07a6451529590a420cb787f6
thegergo02 <thegergo02@tutanota.com>
2019-10-27 09:32:04 +0100

    fix(error): wrong return value

--- error_checking.go
+++ error_checking.go
@@ -7,10 +7,9 @@ import (
 
 func CheckResponseErrors(resp []byte) error {
 	jsonMap := make(map[string]string)
-	err := json.Unmarshal(resp, &jsonMap)
+	json.Unmarshal(resp, &jsonMap)
 	if val, ok := jsonMap["error"]; ok {
 		return errors.New(val)
-	} else {
-		return err
 	}
+	return nil
 }
