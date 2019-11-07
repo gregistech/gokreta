02ba52b203cd103492052bd05f698269d2057ce7
thegergo02 <thegergo02@tutanota.com>
2019-11-07 06:39:44 +0100

    fix: fix useragent

--- useragent.go
+++ useragent.go
@@ -1,9 +1,11 @@
 package gokreta
 
-const USER_AGENT_MIRROR_URL = "https://raw.githubusercontent.com/boapps/kreta-api-mirror/master/user-agent"
-const USER_AGENT_FALLBACK = "Kreta.Ellenorzo/2.9.4.2019101401 (Android; G8341 0.0)"
+const USER_AGENT_REAL = "goenaplo"
+const USER_AGENT_MIRROR_URL = ""
+const USER_AGENT_FALLBACK = ""
 
 func GetUserAgent() (string, error) {
+        return USER_AGENT_REAL
 	body, err := VerboseMakeRequest("GET", USER_AGENT_MIRROR_URL, nil, "")
 	if err != nil {
 		return USER_AGENT_FALLBACK, nil
