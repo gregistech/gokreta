dd5184f6d372986b313776b102c5dab0db9aca3f
thegergo02 <thegergo02@tutanota.com>
2019-11-10 09:14:21 +0100

    fix: fix useragent return value(s)

--- useragent.go
+++ useragent.go
@@ -5,10 +5,10 @@ const USER_AGENT_MIRROR_URL = ""
 const USER_AGENT_FALLBACK = ""
 
 func GetUserAgent() (string, error) {
-	return USER_AGENT_REAL
+	return USER_AGENT_REAL, nil
 	body, err := VerboseMakeRequest("GET", USER_AGENT_MIRROR_URL, nil, "")
 	if err != nil {
-		return USER_AGENT_FALLBACK, nil
+		return USER_AGENT_FALLBACK, err
 	}
 	return string(body), err
 }
