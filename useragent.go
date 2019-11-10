58234b658adacce6d5adba078cb9e4839ba9c73e
thegergo02 <thegergo02@tutanota.com>
2019-11-10 09:09:50 +0100

    fix: fix useragent

--- useragent.go
+++ useragent.go
@@ -1,11 +1,11 @@
 package gokreta
 
-const USER_AGENT_REAL = "goenaplo"
+const USER_AGENT_REAL = "gokreta"
 const USER_AGENT_MIRROR_URL = ""
 const USER_AGENT_FALLBACK = ""
 
 func GetUserAgent() (string, error) {
-        return USER_AGENT_REAL
+	return USER_AGENT_REAL
 	body, err := VerboseMakeRequest("GET", USER_AGENT_MIRROR_URL, nil, "")
 	if err != nil {
 		return USER_AGENT_FALLBACK, nil
