306663a10d637489d1ec6f475ced69312f06ddf8
thegergo02 <thegergo02@tutanota.com>
2019-11-17 12:19:17 +0100

    func(exams): get exams from user

--- exam_test.go
+++ exam_test.go
@@ -5,28 +5,17 @@ import (
 	"testing"
 )
 
-func TestGetAllExamsByDate(t *testing.T) {
-	instituteCode, userName, password, err := getCredetinalsFromFile()
-	authDetails, err := GetAuthDetailsByCredetinals(instituteCode, userName, password)
-	if err != nil {
-		fmt.Println(err)
-		t.Errorf("An error happened while trying to get all the exams by date!")
-		return
-	}
-	accessToken := authDetails.AccessToken
-	exams, err := GetAllExamsByDate(instituteCode, accessToken, "null", "null")
-	if err != nil || exams == nil {
-		fmt.Println(err)
-		t.Errorf("An error happened while trying to get all the exams by date!")
-		return
-	}
-	fmt.Println("TestGetAllExamsByDate passed!")
-	fmt.Println("Result: ")
-	fmt.Println(exams)
-	fmt.Println()
+func TestGetAllExams(t *testing.T) {
+	t.Run("low-level", TGetAllExams)
+	t.Run("high-level", TUserGetAllExams)
 }
 
-func TestGetAllExams(t *testing.T) {
+func TestGetAllExamsByDate(t *testing.T) {
+	t.Run("low-level", TGetAllExamsByDate)
+	t.Run("high-level", TUserGetAllExamsByDate)
+}
+
+func TGetAllExams(t *testing.T) {
 	instituteCode, userName, password, err := getCredetinalsFromFile()
 	authDetails, err := GetAuthDetailsByCredetinals(instituteCode, userName, password)
 	if err != nil {
@@ -41,7 +30,68 @@ func TestGetAllExams(t *testing.T) {
 		t.Errorf("An error happened while trying to get all the exams!")
 		return
 	}
-	fmt.Println("TestGetAllExams passed!")
+	fmt.Println("TGetAllExams passed!")
+	fmt.Println("Result: ")
+	fmt.Println(exams)
+	fmt.Println()
+}
+
+func TGetAllExamsByDate(t *testing.T) {
+	instituteCode, userName, password, err := getCredetinalsFromFile()
+	authDetails, err := GetAuthDetailsByCredetinals(instituteCode, userName, password)
+	if err != nil {
+		fmt.Println(err)
+		t.Errorf("An error happened while trying to get all the exams by date!")
+		return
+	}
+	accessToken := authDetails.AccessToken
+	exams, err := GetAllExamsByDate(instituteCode, accessToken, "null", "null")
+	if err != nil || exams == nil {
+		fmt.Println(err)
+		t.Errorf("An error happened while trying to get all the exams by date!")
+		return
+	}
+	fmt.Println("TGetAllExamsByDate passed!")
+	fmt.Println("Result: ")
+	fmt.Println(exams)
+	fmt.Println()
+}
+
+func TUserGetAllExams(t *testing.T) {
+	instituteCode, userName, password, err := getCredetinalsFromFile()
+	user, err := NewUser(instituteCode, userName, password)
+	if err != nil {
+		fmt.Println(err)
+		t.Errorf("An error happened while trying to get all the exams (from User)!")
+		return
+	}
+	exams, err := user.GetAllExams()
+	if err != nil {
+		fmt.Println(err)
+		t.Errorf("An error happened while trying to get all the exams (from User)!")
+		return
+	}
+	fmt.Println("TUserGetAllExams passed!")
+	fmt.Println("Result: ")
+	fmt.Println(exams)
+	fmt.Println()
+}
+
+func TUserGetAllExamsByDate(t *testing.T) {
+	instituteCode, userName, password, err := getCredetinalsFromFile()
+	user, err := NewUser(instituteCode, userName, password)
+	if err != nil {
+		fmt.Println(err)
+		t.Errorf("An error happened while trying to get all the exams by date (from User)!")
+		return
+	}
+	exams, err := user.GetAllExams()
+	if err != nil {
+		fmt.Println(err)
+		t.Errorf("An error happened while trying to get all the exams by date(from User)!")
+		return
+	}
+	fmt.Println("TUserGetAllExamsByDate passed!")
 	fmt.Println("Result: ")
 	fmt.Println(exams)
 	fmt.Println()
--- user.go
+++ user.go
@@ -16,3 +16,11 @@ func (user User) GetStudentDetails() (Student, error) {
 func (user User) GetStudentDetailsByDate(fromDate, toDate string) (Student, error) {
 	return GetStudentDetailsByDate(user.authDetails.InstituteCode, user.authDetails.AccessToken, fromDate, toDate)
 }
+
+func (user User) GetAllExams() ([]Exam, error) {
+	return GetAllExams(user.authDetails.InstituteCode, user.authDetails.AccessToken)
+}
+
+func (user User) GetAllExamsByDate(fromDate, toDate string) ([]Exam, error) {
+	return GetAllExamsByDate(user.authDetails.InstituteCode, user.authDetails.AccessToken, fromDate, toDate)
+}
